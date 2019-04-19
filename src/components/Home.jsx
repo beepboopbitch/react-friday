2a51641d810d6eb05fe13994f88c4a4af690af93
Nayomi Morita <nayomi.morita@gmail.com>
2019-04-19 12:50:46 -0700

    add styled media queried quote to home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,34 @@ import { Switch, Route, Link } from 'react-router-dom';
 function Home(){
   return(
   <div>
-    <h1>test home</h1>
+    <style jsx>{`
+      .quote{
+        font-size: 30px;
+        text-align: center;
+        margin-top: 30px;
+        margin-left: 10%;
+        margin-right: 10%;
+      }
+      @media screen and (max-width: 900px){
+        
+      }
+      @media screen and (max-width: 640px){
+        .quote{
+          font-size: 25px;
+        }
+      }
+      @media screen and (max-width: 470px){
+        
+      }
+    `}</style>
+    
+    
+    <div className="quote">
+      <p>If it's handcrafted, vintage, custom, or unique, it's on Etsy.</p>
+    </div>
+    
+    
+    
   </div>
   );
 }
