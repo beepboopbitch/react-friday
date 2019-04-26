d69539d6a112689a0c994c29b0c4623943ab6d3a
Nayomi Morita <nayomi.morita@gmail.com>
2019-04-26 08:54:28 -0700

    add to popular

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
 
 class Popular extends React.Component {
-  
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      fromVisibleOnPage: false
+    };
+  }
+
+  render(){
+    return (
+      <div>
+        <p>This is my Popular component</p>
+      </div>
+    );
+  }
 }
+
+export default Popular;
