e93c53b0abbf5a320eea4f356f44d6535c4d2c86
Nayomi Morita <nayomi.morita@gmail.com>
2019-04-28 21:59:42 -0700

    add imports

--- src/components/Pop1.jsx
+++ src/components/Pop1.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import { Switch, Route, Link } from 'react-router-dom';
+import Pop2 from './Pop2';
+import Pop3 from './Pop3';
+import Pop4 from './Pop4';
+import Pop5 from './Pop5';
+import Pop6 from './Pop6';
 
 function Pop1(){
   return(
@@ -20,6 +25,9 @@ function Pop1(){
         <p>Company <input /></p>
         <p>Product <input /></p>
         <p>Price <input /></p>
+      </div>
+      <div>
+        <p><Link to="/pop6">example text</Link></p>
       </div>
     </div>
 
--- src/components/Pop3.jsx
+++ src/components/Pop3.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import { Switch, Route, Link } from 'react-router-dom';
+import Pop1 from './Pop1';
+import Pop2 from './Pop2';
+import Pop4 from './Pop4';
+import Pop5 from './Pop5';
+import Pop6 from './Pop6';
 
 function Pop3(){
   return(
