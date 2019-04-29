8ad4f79f24601491effe90e6befb2050f27cb905
Nayomi Morita <nayomi.morita@gmail.com>
2019-04-28 21:37:39 -0700

    add pop

--- src/components/Pop1.jsx
+++ src/components/Pop1.jsx
@@ -3,9 +3,26 @@ import { Switch, Route, Link } from 'react-router-dom';
 
 function Pop1(){
   return(
-    <div>
+    <div className="main">
       <h1>pop 1</h1>
+      <style jsx>{`
+        .box{
+          border: 2px solid black;
+          width: 400px;
+          padding-left: 20px;
+        }
+        .main{
+          margin-left: 50px;
+        }
+
+      `}</style>
+      <div className="box">
+        <p>Company <input /></p>
+        <p>Product <input /></p>
+        <p>Price <input /></p>
+      </div>
     </div>
+
   );
 }
 
